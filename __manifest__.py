00ed370e62c6e6d43a965a3973f1b06f15402ed1
saw7277-creator <saw7277@gmail.com>
2026-03-23 16:47:26 -0400

    Create __manifest__.py for New Helpdesk app
    Initial manifest file for the New Helpdesk app.

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+
+{
+    "name": "New Helpdesk",
+    "version": "18.0.1.0.0",
+    "summary": "Custom Helpdesk app for Odoo 18 Community",
+    "category": "Services/Helpdesk",
+    "author": "OpenAI",
+    "license": "LGPL-3",
+    "depends": ["mail", "contacts"],
+    "data": [
+        "security/helpdesk_security.xml",
+        "security/ir.model.access.csv",
+        "data/helpdesk_sequence.xml",
+        "data/helpdesk_stage_data.xml",
+        "views/helpdesk_team_views.xml",
+        "views/helpdesk_stage_views.xml",
+        "views/helpdesk_tag_views.xml",
+        "views/helpdesk_sla_views.xml",
+        "views/helpdesk_ticket_views.xml",
+        "views/helpdesk_menus.xml"
+    ],
+    "application": True,
+    "installable": True
+}
